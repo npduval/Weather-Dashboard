b663a6037cdabab162ce78efe29a202582b61c1f
npduval <nickman92186@yahoo.com>
2021-10-20 18:56:40 -0400

    Initial construction of HTML and Javascript

---
+++ script.js
@@ -0,0 +1,43 @@
+const searchCityBtn = document.getElementById('searchCity');
+const cityInput = document.getElementById('city');
+
+function handleSearch(event) {
+    event.preventDefault();
+    let search = cityInput.value;
+
+    if (search) {
+        getLocation(search);
+        //clear city info on page
+    } else {
+        alert("Please enter a valid City, State search")
+    }
+
+};
+  const getLocation = function (search){
+    let requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search + ',USA&limit=1&appid=75e435a88e4bd5b36dbaea785d477577';  //geocodeing API
+
+    fetch(requestUrl)
+    .then (function (responce){
+        if (responce.ok){
+            responce.json()
+    .then (function (data){
+        getWeather(data.location);
+    });
+
+        } else  {
+            alert('Error:' + responce.statusText)
+        }
+    })
+    
+
+  };
+
+   
+    
+
+
+
+
+
+
+searchCityBtn.addEventListener('click', handleSearch);
